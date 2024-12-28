719573a94d21831c89e201e2adb952b5664d6a8e
Faizan <50117959+FaaaiZaaaN@users.noreply.github.com>
2024-12-28 19:05:06 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import Home from "../app/Home/page";
-import About from "./About/page";
-import Cart from "./Cart/page";
-import Contact from "./Contact/page";
-import Product from "./Product/page";
-import Shop from "./Shop/page";
 
 
 
@@ -11,12 +6,6 @@ export default function HomeComponent() {
   return (
     <div className="bg-blue-500">
       <Home />
-      {/* <Contact />
-      <About />
-      <Cart />
-      <Product />
-      <Shop /> */}
-
     </div>
   );
 }
